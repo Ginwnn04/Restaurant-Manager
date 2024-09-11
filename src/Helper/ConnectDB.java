c7d62b7a3cfd4ae0bf4753752fb46bddb5417719
quangdeptrai2 <myemailcls>
2024-09-11 16:41:49 +0700

    update db

--- src/Helper/ConnectDB.java
+++ src/Helper/ConnectDB.java
@@ -9,7 +9,13 @@ import java.util.logging.Logger;
 
 public class ConnectDB {
     private static ConnectDB instance;
-    private String connectionUrl = "jdbc:postgresql://localhost:5432/?user=postgres&password=123456";
+    private String host = "dpg-crgm10aj1k6c739jcud0-a.singapore-postgres.render.com";
+    private String dbName = "testing_restaurant_manager";
+    private String username = "testing_restaurant_manager_user";
+    private String password = "7p3LCOi2nW7LAE9kxlfwSFq3yGKnL1gK";
+    private String sslMode = "&ssl=true";
+    
+    private String connectionUrl = "jdbc:postgresql://" + host + ":5432/" + dbName + "?user=" + username + "&password=" + password + sslMode;
     private Connection con;
     
     private ConnectDB() {
